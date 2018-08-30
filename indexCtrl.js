b1cc128d0dc60f4666b1b308ddbed46bc0b3e7b3
eladforgash <elad.forgash@gmail.com>
2018-08-30 16:50:44 +0200

    towards uploading my 2 cities

--- indexCtrl.js
+++ indexCtrl.js
@@ -3,78 +3,44 @@ weatherApp.controller('indexCtrl', function($scope, jsonSer){
     // calling for loading and passing the cities info from the json files:    
     jsonSer.load();
     $scope.cities=jsonSer.citiesArray;
-    
-        
+    var locatedCity;
+    $scope.isWrong=false;
+
+      
+       
     //getting the user cityInput and publishing its details:
     $scope.showCityDetails= function(cityEntered){
         if (!cityEntered){
             $scope.isCity=false;
         }else{
         //locating the city in the citiesArray:
-        compareCities(cityEntered);
-        changeCityName();
-        setCityLink();
-        setCityTemp();
-        setCityImage();
-        setCityHumid();
-        setCityWind();
-        $scope.cityInput='';
-                
+        compareCities(cityEntered)        
+        $scope.userCity=locatedCity;
+        $scope.cityInput='';        
         }
     }    
     
 
     function compareCities(cityEntered){
 
-        $scope.locatedCity=$scope.cities.find(function(city){
+        locatedCity=$scope.cities.find(function(city){
             
             city.city=city.city.toLowerCase();
             cityEntered=cityEntered.toLowerCase();
             return city.city == cityEntered;
         });
-    }
-
-
-    // functions for adjusting the locatedCity details for publidshing:
-    // uppercase first letter of city name:
-    function changeCityName(){
-        $scope.isCity=true;
-        $scope.locatedCity.city=$scope.locatedCity.city.charAt(0).toUpperCase()+$scope.locatedCity.city.substr(1);
-        
-    }
-
-    //setting city link:
-    function setCityLink(){
-      
-        $scope.locatedCityLink=$scope.locatedCity.link;
-    }
-
-    //setting city temperature:
-    function setCityTemp(){
-       
-        $scope.locatedCityTemp=$scope.locatedCity.temp;
-    }
-
-
-    //setting city image:
-    function setCityImage(){
-      
-        $scope.locatedCityImage=$scope.locatedCity.img;
-    }
-
-    //setting city humiditiy:
-    function setCityHumid(){
-       
-        $scope.locatedCityHumid=$scope.locatedCity.humidity;
-    }
-    
-    //setting city wind:
-    function setCityWind(){
-        if($scope.locatedCity.wind){
-        var windIndex=$scope.locatedCity.wind.search('Wind');
-        $scope.locatedCityWind=$scope.locatedCity.wind.slice(windIndex);
+        if(locatedCity==undefined){            
+            $scope.isCity=false; 
+            $scope.isWrong=true;           
+            return locatedCity;
         }else{
-            $scope.locatedCityWind='';
+            $scope.isCity=true;
+            $scope.isWrong=false;
+            locatedCity.city=locatedCity.city.charAt(0).toUpperCase()+locatedCity.city.substr(1);
+            return locatedCity;
         }
     }
+
+
+    
 });
--- jsonSer.js
+++ jsonSer.js
@@ -2,9 +2,19 @@
 
 weatherApp.factory('jsonSer', function($http, $q){
 
+    //constructor of city objects:
+    var City = function(cityObj){
+        this.city = changeCityName(cityObj);
+        this.link = setCityLink(cityObj);
+        this.temp = setCityTemp(cityObj);
+        this.img = setCityImage(cityObj);
+        this.humidity = setCityHumid(cityObj);
+        this.wind = setCityWind(cityObj);
+    }
     // array of cities data:
     var citiesArray=[];
     //var wasEverLoaded=false;
+    
     
     
     // uploading cities with a $q load function:
@@ -36,7 +46,7 @@ weatherApp.factory('jsonSer', function($http, $q){
             for(var i=0; i<response.data.length; i++){
                 let city = response.data[i];
                 
-                citiesArray.push((city)); 
+                citiesArray.push(new City(city)); 
                 
                   
                 }          
@@ -49,12 +59,60 @@ weatherApp.factory('jsonSer', function($http, $q){
                 async.reject();
             }
         );
-
+        
     }
-    // the service object access:
-   return {
-       load:load,
-       citiesArray:citiesArray
-   }
+    // console.log(citiesArray);
+    
 
+
+   // functions for adjusting the locatedCity details for publidshing:
+    // uppercase first letter of city name:
+    function changeCityName(cityObj){
+       
+        cityObj.city=cityObj.city.charAt(0).toUpperCase()+cityObj.city.substr(1);
+        return cityObj.city;
+    }
+
+    //setting city link:
+    function setCityLink(cityObj){
+      
+        return cityObj.link;
+    }
+
+    //setting city temperature:
+    function setCityTemp(cityObj){
+       
+        return cityObj.temp;
+    }
+
+
+    //setting city image:
+    function setCityImage(cityObj){
+      
+        return cityObj.img;
+    }
+
+    //setting city humiditiy:
+    function setCityHumid(cityObj){
+       
+        return cityObj.humidity;
+    }
+    
+    //setting city wind:
+    function setCityWind(cityObj){
+        if(cityObj.wind){
+        var windIndex=cityObj.wind.search('Wind');
+        locatedCityWind=cityObj.wind.slice(windIndex);
+        return locatedCityWind;
+        }else{
+            return locatedCityWind='';
+        }
+    }
+
+
+
+    return {
+        load:load,
+        citiesArray:citiesArray
+    }
 });
